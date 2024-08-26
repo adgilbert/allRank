bdc1e59733961e0b1c6744124383b6a0c7743bf2
Andrew Gilbert <andrew.gilbert@irhythmtech.com>
2024-08-26 19:05:27 +0000

    remove max versions

--- setup.py
+++ setup.py
@@ -7,16 +7,16 @@ HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
 
 reqs = [
-    "scikit-learn>=1.1.0, <=1.2.1",
-    "pandas>=1.0.5, <=1.3.5",
-    "numpy>=1.18.5, <=1.21.6",
-    "scipy>=1.4.1, <=1.7.3",
+    "scikit-learn>=1.1.0",
+    "pandas>=1.0.5",
+    "numpy>=1.18.5",
+    "scipy>=1.4.1",
     "attrs>=19.3.0",
     "flatten_dict>=0.3.0",
     "tensorboardX>=2.1.0",
-    "gcsfs==0.6.2",
+    "gcsfs>=0.6.2",
     "google-auth>=2.15.0",
-    "fsspec <= 2023.1.0"
+    "fsspec"
 ]
 
 setup(
